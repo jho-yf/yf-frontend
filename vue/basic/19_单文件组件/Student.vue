c9c003fa166439c6b9838c6dd6fe60818da82e9e
jho-yf <xu-jihong@qq.com>
2024-03-02 21:31:28 +0800

    feat: 生命周期 + 组件

---
+++ vue/basic/19_单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- 组件结构 -->
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学校年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关代码（数据、方法等）
+    export default {
+        name: 'School',
+        data() {
+            return {
+                name: '张三',
+                age: 18
+            }
+        }
+    }
+</script>
