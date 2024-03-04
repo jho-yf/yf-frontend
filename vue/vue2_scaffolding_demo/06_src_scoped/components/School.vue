e95cac7270172de6b18e6d55c170eee8931f9272
jho-yf <xu-jihong@qq.com>
2024-03-04 22:35:35 +0800

    feat: scoped

---
+++ vue/vue2_scaffolding_demo/06_src_scoped/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="demo">
+        <h2>学校姓名：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+
+        data() {
+            return {
+                name: '北京大学',
+                address: '北京市',
+            };
+        },
+    };
+</script>
+
+<style scoped>
+    .demo {
+        color: skyblue
+    }
+</style>
