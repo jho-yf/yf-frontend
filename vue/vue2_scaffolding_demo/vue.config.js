4aa278871ee2a6990bebc1884acd912ad4783048
jho-yf <xu-jihong@qq.com>
2024-05-12 15:02:13 +0800

    feat: 代理配置

--- vue/vue2_scaffolding_demo/vue.config.js
+++ vue/vue2_scaffolding_demo/vue.config.js
@@ -2,5 +2,26 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭语法检查
-  lintOnSave: false
+  lintOnSave: false,
+  // 开启代理服务器 方式一
+  // devServer: {
+  //   proxy: 'http://localhost:3000'
+  // }
+  // 开启代理服务器 方式二
+  devServer: {
+    proxy: {
+      '/server1': {
+        target: 'http://localhost:3000',
+      },
+      '/server2': {
+        target: 'http://localhost:3001',
+        // 用于重写请求路径
+        // pathRewrite: {'^/server2': ''},
+        // 用于支持websocket
+        // ws: true,
+        // 用于控制请求头中的host值
+        // changeOrigin: true
+      }
+    }
+  }
 })
