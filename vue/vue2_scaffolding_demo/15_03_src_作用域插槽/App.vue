26a31260ae5fbe2a9c195de512e64e694e396f75
jho-yf <xu-jihong@qq.com>
2024-05-12 17:25:09 +0800

    feat: 作用域插槽

---
+++ vue/vue2_scaffolding_demo/15_03_src_作用域插槽/App.vue
@@ -0,0 +1,46 @@
+<<template>
+    <div class="container">
+        <Category title="无序列表">
+            <template scope="data">
+                <ul>
+                    <li v-for="(item, index) in data.games" :key="index">{{ item }}</li>
+                </ul>
+            </template>
+        </Category>
+        <Category title="有序列表">
+            <template scope="{ games }">
+                <ol>
+                    <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+                </ol>
+            </template>
+        </Category>
+        <Category title="H6">
+            <template scope="{ games }">
+                <h6 v-for="(item, index) in games" :key="index">{{ item }}</h6>
+            </template>
+        </Category>
+    </div>
+</template>
+
+<script>
+    import Category from './components/Category.vue';
+    export default {
+        name: 'App',
+        components: {
+            Category,
+        },
+        data() {
+            return {
+            };
+        },
+        methods: {
+        }
+    };
+</script>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
---
+++ vue/vue2_scaffolding_demo/15_03_src_作用域插槽/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category">
+    <h1>{{title}}</h1>
+    <slot :games="games">默认插槽内容</slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Category',
+        props: ['title'],
+        data() {
+          return {
+            games: ["刀塔", "英雄联盟", "守望先锋", "王者荣耀"]
+          }
+        }
+    };
+</script>
+
+<style scoped>
+    .category{
+        background-color: blueviolet;
+        width: 300px;
+        height: 300px;
+    }
+</style>
