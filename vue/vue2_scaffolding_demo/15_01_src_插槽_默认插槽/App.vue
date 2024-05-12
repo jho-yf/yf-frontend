8297fb92a45fa32771ceee22fd820a34e50147ef
jho-yf <xu-jihong@qq.com>
2024-05-12 16:39:34 +0800

    feat: 默认插槽

---
+++ vue/vue2_scaffolding_demo/15_01_src_插槽_默认插槽/App.vue
@@ -0,0 +1,40 @@
+<<template>
+    <div class="container">
+        <Category title="食物">
+            <h2>无</h2>
+        </Category> 
+        <Category title="电影">
+            <img src="https://avatars.githubusercontent.com/u/25262160?v=4" />
+        </Category> 
+        <Category title="音乐">
+            <ul>
+                <li v-for="(item, index) in music" :key="index">{{item}}</li>
+            </ul>
+        </Category> 
+        <Category />
+    </div>
+</template>
+
+<script>
+    import Category from './components/Category.vue';
+    export default {
+        name: 'App',
+        components: {
+            Category,
+        },
+        data() {
+            return {
+                music: ["周杰伦","林俊杰"]
+            };
+        },
+        methods: {
+        }
+    };
+</script>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
---
+++ vue/vue2_scaffolding_demo/15_01_src_插槽_默认插槽/components/Category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="category">
+    <h1>{{title}}</h1>
+    <!-- 定义插槽 -->
+    <slot><h1>默认插槽</h1></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Category',
+        props: ['title']
+    };
+</script>
+
+<style scoped>
+    .category{
+        background-color: red;
+        width: 300px;
+        height: 300px;
+    }
+</style>
