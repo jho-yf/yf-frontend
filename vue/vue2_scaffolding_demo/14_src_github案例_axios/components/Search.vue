4ea99e2579644800fd6c9d4a6ffe7fbaeaef00d9
jho-yf <xu-jihong@qq.com>
2024-05-12 15:01:23 +0800

    feat: github案例 axios

---
+++ vue/vue2_scaffolding_demo/14_src_github案例_axios/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+            <input type="text" placeholder="enter the name you search" v-model="keyword" />
+            &nbsp;
+            <button @click="search">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+
+        name: 'Search',
+        data() {
+            return {
+                keyword: ''
+            };
+        },
+        methods: {
+            search() {
+                axios.get(`https://api.github.com/search/users?q=${this.keyword}`).then(resp => {
+                    this.$bus.$emit('searchResult', resp.data.items);
+                });
+            }
+        }
+    };
+</script>
