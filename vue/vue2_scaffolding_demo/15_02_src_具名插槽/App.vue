bfd06ec774ef71252dbeb468511aeabc9ee08b05
jho-yf <xu-jihong@qq.com>
2024-05-12 17:04:30 +0800

    feat: 具名插槽

---
+++ vue/vue2_scaffolding_demo/15_02_src_具名插槽/App.vue
@@ -0,0 +1,48 @@
+<<template>
+    <div class="container">
+        <Category title="食物">
+            <h2 slot="center">无</h2>
+            <a slot="footer" href="http://www.baidu.com">更多</a>
+        </Category> 
+        <Category title="电影">
+            <img slot="center" src="https://avatars.githubusercontent.com/u/25262160?v=4" />
+            <div slot="footer">
+                <a href="http://www.baidu.com">百度搜素</a>&nbsp;&nbsp;&nbsp;
+                <a href="http://www.baidu.com">豆瓣搜索</a>&nbsp;&nbsp;&nbsp;
+                <a href="http://www.baidu.com">更多</a>
+            </div>
+        </Category> 
+        <Category title="音乐">
+            <template v-slot:center>
+                <ul>
+                    <li v-for="(item, index) in music" :key="index">{{item}}</li>
+                </ul>
+            </template>
+        </Category> 
+        <Category />
+    </div>
+</template>
+
+<script>
+    import Category from './components/Category.vue';
+    export default {
+        name: 'App',
+        components: {
+            Category,
+        },
+        data() {
+            return {
+                music: ["周杰伦","林俊杰"]
+            };
+        },
+        methods: {
+        }
+    };
+</script>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
---
+++ vue/vue2_scaffolding_demo/15_02_src_具名插槽/components/Category.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="category">
+    <h1>{{title}}</h1>
+    <!-- 定义插槽 -->
+    <slot name="center"><h1>默认插槽</h1></slot>
+    <slot name="footer"><h1>默认插槽</h1></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Category',
+        props: ['title']
+    };
+</script>
+
+<style scoped>
+    .category{
+        background-color: blueviolet;
+        width: 300px;
+        height: 300px;
+    }
+</style>
