ffd124028ababd4f1f5c33e1b24a37f764c82c45
jho-yf <xu-jihong@qq.com>
2024-05-18 13:10:27 +0800

    feat: vue router的基本使用

---
+++ vue/vue2_scaffolding_demo/20_src_vue-router的基本使用/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Home 内容</h2>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+    };
+</script>
---
+++ vue/vue2_scaffolding_demo/20_src_vue-router的基本使用/router/index.js
@@ -0,0 +1,16 @@
+import VueRouter from 'vue-router'
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
