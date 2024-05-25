fc0c5c9286ee070edd126798b2d53117c5f39537
jho <xu-jihong@qq.com>
2024-05-25 20:26:00 +0800

    feat: ref

---
+++ vue/vue3_demo_vite/04_src_ref/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Vue 3</h1>
+  <Person></Person>
+</template>
+
+<script lang="ts">
+  import Person from './components/Person.vue'
+
+  export default {
+    name: 'App',
+    components: { Person }
+  }
+</script>
+
+<style scoped>
+</style>
