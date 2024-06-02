5cc71a444445e6cfb594b76d6562525146a04a84
jho <xu-jihong@qq.com>
2024-06-02 09:31:09 +0800

    feat: teleport

---
+++ vue/vue3_demo_vite/30_src_teleport/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="isShow = true">展示弹窗</button>
+  <!-- 传送到body标签中 -->
+  <teleport to="body">
+    <div class="modal" v-show="isShow">
+        <h2>弹窗标题</h2>
+        <p>弹窗内容</p>
+        <button @click="isShow = false">关闭弹窗</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    let isShow = ref(false)
+</script>
+
+<style scoped>
+    .modal {
+        width: 300px;
+        height: 150px;
+        background-color: gray;
+        text-align: center;
+        position: fixed;
+        left: 50%;
+        margin-left: -150px;
+        top: 30px;
+    }
+</style>
